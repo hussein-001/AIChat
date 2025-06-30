129be339af038981fdbde46ab823ca8e0d0a8983
HusseinAwada001 <husseinawada001@icloud.com>
2025-06-30 17:06:06 +0300

    Initial Commit

---
+++ AIChat/AIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatApp.swift
+//  AIChat
+//
+//  Created by Hussien Awada on 30/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct AIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIChatTests/AIChatTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatTests.swift
+//  AIChatTests
+//
+//  Created by Hussien Awada on 30/06/2025.
+//
+
+import Testing
+@testable import AIChat
+
+struct AIChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
