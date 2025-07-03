86b0d5dfbe3b76d82ab781808a81751e8e85c8a0
HusseinAwada001 <husseinawada001@icloud.com>
2025-07-03 16:27:15 +0300

    [Feature] OnboardingCompletedView

--- AIChat/Core/Onboarding/CompletedView/OnboardingCompletedView.swift
+++ AIChat/Core/Onboarding/CompletedView/OnboardingCompletedView.swift
@@ -11,24 +11,55 @@ struct OnboardingCompletedView: View {
     
     @Environment(AppState.self) private var root
     
+    @State private var isCompleteingProfileSetup: Bool = false
+    var selectedColor: Color = .orange
+    
     var body: some View {
-        VStack {
-            Text("OnboardingCompleted!")
-                .frame(maxHeight: .infinity)
+        VStack(alignment: .leading, spacing: 12) {
+            Text("Setup complete!")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundStyle(selectedColor)
             
-            Button {
-                // finish onBoarding and enter app!
-                onFinishButtonPressed()
-            } label: {
-                Text("Finish")
-                    .callToActionButton()
-            }
+            Text("We've set up your profile and you're ready to start chatting.")
+                .font(.title)
+                .fontWeight(.medium)
+                .foregroundStyle(.secondary)
         }
-        .padding(16)
+        .frame(maxHeight: .infinity)
+        .safeAreaInset(edge: .bottom, alignment: .center, content: {
+            ctaButton
+        })
+        .padding(24)
+        .toolbar(.hidden, for: .navigationBar)
     }
     
     func onFinishButtonPressed() {
-        root.updateViewState(showTabBarView: true)
+        isCompleteingProfileSetup = true
+        Task {
+            try await Task.sleep(for: .seconds(3))
+            isCompleteingProfileSetup = false
+            root.updateViewState(showTabBarView: true)
+            
+        }
+    }
+    
+    private var ctaButton: some View {
+        Button {
+            // finish onBoarding and enter app!
+            onFinishButtonPressed()
+        } label: {
+            ZStack {
+                if isCompleteingProfileSetup {
+                    ProgressView()
+                        .tint(.white)
+                } else {
+                    Text("Finish")
+                }
+            }
+            .callToActionButton()
+        }
+        .disabled(isCompleteingProfileSetup)
     }
 }
 
